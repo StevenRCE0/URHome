e44dcf10e0fa65ab5e8ca07776b8fb07eb6659e4
StevenRCE0 <40051361+StevenRCE0@users.noreply.github.com>
2022-06-08 23:25:30 +0800

    Patch

--- service.js
+++ service.js
@@ -1,45 +1,47 @@
-import http from "http"
+import http from 'http'
 
 let cachedConditions = {
-  URLight: {
-    on: false,
-    brightness: 0,
-    temperature: 0,
-  },
+    URLight: {
+        on: false,
+        brightness: 0,
+        temperature: 0,
+    },
 }
 
-http
-  .createServer((req, res) => {
-    requestURL = new URL(req.url)
-    requestPath = requestURL.pathname.split("/")
-    if (requestPath[0] === "light") {
-      result =
-        lightResponder(
-          requestPath[1],
-          requestURL.search
-        ) ?? "OK"
-      res.writeHead(200, { "Content-Type": "text/plain" })
-      res.end(result)
+http.createServer((req, res) => {
+    const requestURL = new URL(req.url, `http://${req.headers.host}`)
+    const requestPath = requestURL.pathname.split('/')
+    if (requestPath[1] === 'light') {
+        const result =
+            lightResponder(requestPath[2], requestURL.search.substring(1)) ??
+            'OK'
+        res.writeHead(200, { 'Content-Type': 'text/plain' })
+        res.end(result)
     }
-  })
-  .listen(8700)
+}).listen(8700)
 
 const lightResponder = (type, value) => {
-  console.log(type, value)
-  if (type === "on") {
-    if (value !== undefined) {
-      // TODO: Johnny here
-      cachedConditions["URLight"].on = value
-    } else {
-      return cachedConditions["URLight"].on
+    console.log(type, typeof value, value.length)
+    if (type === 'on') {
+        if (value.length > 0) {
+            // TODO: Johnny here
+            if (value === 'on') {
+                cachedConditions.URLight.on = true
+            }
+            if (value === 'off') {
+                cachedConditions.URLight.on = false
+            }
+            console.log(cachedConditions)
+        } else {
+            return JSON.stringify(cachedConditions['URLight'].on)
+        }
     }
-  }
-  if (type === "brightness") {
-    if (value !== undefined) {
-      // TODO: Johnny here
-      cachedConditions["URLight"].brightness = value
-    } else {
-      return cachedConditions["URLight"].brightness
+    if (type === 'brightness') {
+        if (value.length > 0) {
+            // TODO: Johnny here
+            cachedConditions['URLight'].brightness = value
+        } else {
+            return JSON.stringify(cachedConditions['URLight'].brightness)
+        }
     }
-  }
 }
